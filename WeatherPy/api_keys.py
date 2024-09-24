c704cd995335d44a9d6d80a547c633f1bfe07951
Ariana Breck <angelsullivan@Angels-Air.attlocal.net>
2024-09-23 19:37:38 -0500

    Completed

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "60e028e95039dd3fd74fd941d1f195d7"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "e413850548804aa5903bee3e4b5794a9"
